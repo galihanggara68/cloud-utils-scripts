1c0390845b5df2fd7a11d7e83f40d7432812f4b0
galihanggara68 <galihanggara68@gmail.com>
2022-09-29 16:07:09 +0700

    * Change wget to curl

--- updater.sh
+++ updater.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Updating . . ."
-rm -r configmap.sh backup-configmap.sh rollout.sh
-wget "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/configmap.sh"
-wget "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/backup-configmap.sh"
-wget "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/rollout.sh"
+rm -f configmap.sh backup-configmap.sh rollout.sh
+sleep 1
+curl -O "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/configmap.sh"
+curl -O "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/backup-configmap.sh"
+curl -O "https://raw.githubusercontent.com/galihanggara68/cloud-utils-scripts/master/rollout.sh"
 echo "Done."
